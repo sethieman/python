e0d7f7fc93b36876b9bd1a95e2d2e8c56f7adfa0
sethieman <sthieman@unomaha.edu>
2023-11-19 19:09:24 -0600

    Update television.py

--- television.py
+++ television.py
@@ -5,46 +5,35 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-<<<<<<< Updated upstream
-    def __init__(self):
-=======
     def __init__(self) -> None:
         '''All default instance variables are set in this method'''
->>>>>>> Stashed changes
         
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
-<<<<<<< Updated upstream
-    def power(self):
-=======
+
     def power(self) -> None:
         '''This method is used to turn the tv on and off by changing the
             value of the status variable.'''
->>>>>>> Stashed changes
+
 
         self.__status = not self.__status
 
-<<<<<<< Updated upstream
-    def mute(self):
-=======
+
     def mute(self) -> None:
         '''This method is used to mute and unmute the tv when it is on
             by changing the value of the muted variable.'''
->>>>>>> Stashed changes
+
 
         if self.__status:
             self.__muted = not self.__muted
 
-<<<<<<< Updated upstream
-    def channel_up(self):
-=======
+
     def channel_up(self) -> None:
         '''This method is used to increase the tv channel. If it is at the maximum,
             then it loops back around to the minimum channel.'''
->>>>>>> Stashed changes
 
         if self.__status: 
             if self.__channel == Television.MAX_CHANNEL:
@@ -52,13 +41,9 @@ class Television:
             else:
                 self.__channel += 1
     
-<<<<<<< Updated upstream
-    def channel_down(self):
-=======
     def channel_down(self) -> None:
         '''This method is used to decrease the tv channel. If it is at the minimum,
             then it loops around to the maximum channel.'''
->>>>>>> Stashed changes
 
         if self.__status:
             if self.__channel == Television.MIN_CHANNEL:
@@ -66,13 +51,10 @@ class Television:
             else:
                 self.__channel -= 1
 
-<<<<<<< Updated upstream
-    def volume_up(self):
-=======
+
     def volume_up(self) -> None:
         '''This method is used to turn the volume up to at most the MAX_VOLUME, when the
             tv is on. If the tv was muted, changing the volume will unmite it.'''
->>>>>>> Stashed changes
 
         if self.__status:
             if self.__volume == Television.MAX_VOLUME and self.__muted:
@@ -82,13 +64,10 @@ class Television:
                 self.__volume +=1
                 self.__muted = False
 
-<<<<<<< Updated upstream
-    def volume_down(self):
-=======
+
     def volume_down(self) -> None:
         '''This method is used to turn the volume down to at least the MIN_VOLUME, when the
             tv is on. If the tv was muted, changing the volume will unmite it.'''
->>>>>>> Stashed changes
 
         if self.__status:
             if self.__volume == Television.MIN_VOLUME and self.__muted:
@@ -98,12 +77,8 @@ class Television:
                 self.__volume -=1
                 self.__muted = False   
 
-<<<<<<< Updated upstream
-    def __str__(self):
-=======
     def __str__(self) -> str:
         '''This methods sends the values of the tv object to be displayed.'''
->>>>>>> Stashed changes
 
         if self.__muted:
             current_volume = MIN_VOLUME
