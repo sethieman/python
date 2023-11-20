84115541a7a3005999e0a472a8a863a083f0663a
sethieman <sthieman@unomaha.edu>
2023-11-19 19:43:48 -0600

    Update television.py

--- television.py
+++ television.py
@@ -5,7 +5,7 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
         '''All default instance variables are set in this method'''
         
         self.__status = False
@@ -13,20 +13,20 @@ class Television:
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
         '''This method is used to turn the tv on and off by changing the
             value of the status variable.'''
 
         self.__status = not self.__status  
 
-    def mute(self):
+    def mute(self) -> None:
         '''This method is used to mute and unmute the tv when it is on
             by changing the value of the muted variable.'''
 
         if self.__status:
             self.__muted = not self.__muted 
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         '''This method is used to increase the tv channel. If it is at the maximum,
             then it loops back around to the minimum channel.'''
 
@@ -36,7 +36,7 @@ class Television:
             else:
                 self.__channel += 1
     
-    def channel_down(self):
+    def channel_down(self) -> None:
         '''This method is used to decrease the tv channel. If it is at the minimum,
             then it loops around to the maximum channel.'''
 
@@ -46,9 +46,9 @@ class Television:
             else:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         '''This method is used to turn the volume up to at most the MAX_VOLUME, when the
-            tv is on. If the tv was muted, changing the volume will unmite it.'''
+            tv is on. If the tv was muted, changing the volume will unmute it.'''
 
         if self.__status:
             if self.__volume == Television.MAX_VOLUME and self.__muted:
@@ -58,9 +58,9 @@ class Television:
                 self.__volume +=1
                 self.__muted = False
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         '''This method is used to turn the volume down to at least the MIN_VOLUME, when the
-            tv is on. If the tv was muted, changing the volume will unmite it.'''
+            tv is on. If the tv was muted, changing the volume will unmute it.'''
 
         if self.__status:
             if self.__volume == Television.MIN_VOLUME and self.__muted:
@@ -70,8 +70,8 @@ class Television:
                 self.__volume -=1
                 self.__muted = False
 
-    def __str__(self):
-        '''This methods sends the values of the tv object to be displayed.'''
+    def __str__(self) -> str:
+        '''This method is used to send the values of the tv object to be displayed.'''
 
         if self.__muted:
             current_volume = 0
